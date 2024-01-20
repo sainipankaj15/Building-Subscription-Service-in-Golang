f6269d53a06079cff6c897c3a97bb5ad20ef160a
root <root@saini-pc>
2024-01-20 10:56:50 +0530

    Doceker Setup Ready and all necessary go modules installed

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = ":80"
+
+func main(){
+	// connecting database
+
+	// Simple web application : Page will render from server side = Creating sessions
+
+	// creating channels
+
+	// creating waitgroup 
+
+	// setp the application config
+	
+	// When user takes a subscirption , sends emails : Will use gorutine probabaly for this 
+
+	// listeing for web connections 
+
+}
