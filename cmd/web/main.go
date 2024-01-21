6474d0bd7f335ce0b97d79bd930c7b990b6f1468
sainipankaj15 <pksmks1999@gmail.com>
2024-01-21 09:39:43 +0530

    Session and Redis Pool Added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
 )
 
 const webPort = ":80"
 
 func main(){
+
 	// connecting database
-
 	db := intiDB()
-
 	db.Ping()
-
-	// Simple web application : Page will render from server side = Creating sessions
+ 
+	// Creating session 
+	session := initSession()
 
 	// creating channels
 
@@ -36,4 +43,28 @@ func intiDB() *sql.DB {
 	}
 
 	return conn
+}
+
+func initSession() *scs.SessionManager{
+	
+	session := scs.New()
+
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24*time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+
+}
+
+func initRedis() *redis.Pool{
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error)  {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPool
 }
