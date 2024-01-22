54ee48641e210e39fff1dcee190dd7b96ea7729b
sainipankaj15 <pksmks1999@gmail.com>
2024-01-22 22:32:33 +0530

    Mail and Message part done: Mailer.go

---
+++ cmd/web/mailer.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+	"text/template"
+	"time"
+
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+	WaitGroup   *sync.WaitGroup
+	MailerChan  chan Message
+	ErrorChan   chan error
+	DoneChan    chan bool
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Attachments []string
+	Data        any
+	DataMap     map[string]any
+	Template    string
+}
+
+// A function who is listener on the MailerChan
+func (m *Mail) sendMail(msg Message, errorChan chan error) {
+
+	if msg.Template == "" {
+		msg.Template = "mail"
+	}
+
+	if msg.From == "" {
+		msg.From = m.FromAddress
+	}
+
+	if msg.FromName == "" {
+		msg.FromName = m.FromName
+	}
+
+	data := map[string]any{
+		"message": msg.Data,
+	}
+
+	msg.DataMap = data
+
+	//bulid html mail
+	formattedMessage, err := m.bulidHTMLMessage(msg)
+	if err != nil {
+		errorChan <- err
+	}
+
+	//bulid plain text mail
+	plainMessage, err := m.bulidPlainTextMessage(msg)
+	if err != nil {
+		errorChan <- err
+	}
+
+	// We have to create a mail server
+	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	server.Encryption = m.getEncrption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	smtpClient, err := server.Connect()
+	if err != nil {
+		errorChan <- err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.From).AddTo(msg.To).SetSubject(msg.Subject)
+
+	email.SetBody(mail.TextPlain, plainMessage)
+	email.AddAlternative(mail.TextHTML, formattedMessage)
+
+	if len(msg.Attachments) > 0 {
+		for _, val := range msg.Attachments {
+			email.AddAttachment(val)
+		}
+	}
+
+	err = email.Send(smtpClient)
+	if err != nil {
+		errorChan <- err
+	}
+}
+
+func (m *Mail) bulidHTMLMessage(msg Message) (string, error) {
+
+	templateToRender := fmt.Sprintf("./cmd/web/templates/%s.html.gohtml",msg.Template)
+
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", nil
+	}
+
+	var tpl bytes.Buffer
+
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", nil
+	}
+
+	formattedMessageReady := tpl.String()
+	formattedMessageReady, err = m.inlineCSS(formattedMessageReady)
+	if err != nil {
+		return "", nil
+	}
+
+	return formattedMessageReady, nil
+}
+
+func (m *Mail) bulidPlainTextMessage(msg Message) (string, error) {
+
+	templateToRender := fmt.Sprintf("./cmd/web/templates/%s.plain.gohtml",msg.Template)
+
+	t, err := template.New("email-plain").ParseFiles(templateToRender)
+	if err != nil {
+		return "", nil
+	}
+
+	var tpl bytes.Buffer
+
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", nil
+	}
+
+	plainMessage := tpl.String()
+	return plainMessage, nil
+}
+
+func (m *Mail) inlineCSS(s string) (string, error) {
+
+	options := premailer.Options{
+		RemoveClasses:     false,
+		CssToAttributes:   false,
+		KeepBangImportant: true,
+	}
+
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		return "", err
+	}
+
+	html, err := prem.Transform()
+	if err != nil {
+		return "", err
+	}
+
+	return html, nil
+}
+
+func (m *Mail) getEncrption(e string) mail.Encryption {
+
+	switch e {
+	case "tls":
+		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return mail.EncryptionSSLTLS
+	case "none":
+		return mail.EncryptionNone
+	default:
+		return mail.EncryptionSTARTTLS
+	}
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module building-subscritpion-service
 go 1.21.1
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
 	github.com/alexedwards/scs/redisstore v0.0.0-20231113091146-cef4b05350c8 // indirect
 	github.com/alexedwards/scs/v2 v2.7.0 // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.11 // indirect
 	github.com/gomodule/redigo v1.8.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -15,6 +18,11 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/pgx/v4 v4.18.1 // indirect
+	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
+	github.com/vanng822/css v1.0.1 // indirect
+	github.com/vanng822/go-premailer v1.20.2 // indirect
+	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
