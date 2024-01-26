3998cdace43362e17541d9e77862a1b604090fc1
sainipankaj15 <pksmks1999@gmail.com>
2024-01-26 16:50:31 +0530

    Mail sending while first time Register : Done

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"building-subscritpion-service/data"
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "home.page.gohtml", nil)
@@ -28,51 +33,50 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		app.Session.Put(r.Context(), "error", "Invalid Credentials")
-		http.Redirect(w,r,"/login",http.StatusSeeOther)
-		return 
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
 	}
 
 	// if code is there it means user is in our database : Now will verify for password
-	validPassword , err := user.PasswordMatches(password)
+	validPassword, err := user.PasswordMatches(password)
 	if err != nil {
 		app.Session.Put(r.Context(), "error", "Invalid Credentials")
-		http.Redirect(w,r,"/login",http.StatusSeeOther)
-		return 
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
 	}
 
 	if !validPassword {
 		msg := Message{
-			To: email,
+			To:      email,
 			Subject: "Failed log in attempt",
-			Data:"Invalid Login Attempt",
+			Data:    "Invalid Login Attempt",
 		}
 		app.sendEmail(msg)
-		 
+
 		app.Session.Put(r.Context(), "error", "Invalid Credentials")
-		http.Redirect(w,r,"/login",http.StatusSeeOther)
-		return 
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
 	}
-	
-	// if all okay till there : it means user log in : Saving the information in Session 
+
+	// if all okay till there : it means user log in : Saving the information in Session
 	app.Session.Put(r.Context(), "userID", user.ID)
 	app.Session.Put(r.Context(), "user", user)
-	
-	
+
 	app.Session.Put(r.Context(), "flash", "Successfully Login")
 
 	//Redirect to home page
-	http.Redirect(w, r, "/",http.StatusSeeOther)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 
 }
 
 func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
-	// clean up session 
+	// clean up session
 	// Two steps 1. Destory entire session and renew session
 	_ = app.Session.Destroy(r.Context())
 	_ = app.Session.RenewToken(r.Context())
 
 	//Redirect to home page
-	http.Redirect(w, r, "/login",http.StatusSeeOther)
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
@@ -80,6 +84,52 @@ func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	// TODO : Validate - All Data which is very necessary will do later
+
+	//Create a user
+	user := data.User{
+		Email:     r.Form.Get("email"),
+		FirstName: r.Form.Get("first-name"),
+		LastName:  r.Form.Get("last-name"),
+		Password:  r.Form.Get("password"),
+		Active:    0,
+		IsAdmin:   0,
+	}
+
+	_, err = user.Insert(user)
+
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Unable to create this user")
+
+		//Redirect to home page
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+		return
+	}
+
+	// Sending an activation Email
+	url := fmt.Sprintf("http://localhost/activation?email=%s", user.Email)
+
+	// This is our url but now we will use only Signed URL to avoid hackign and tempering the URL : Signer.go
+	signedURL := GenerateTokenFromString(url)
+	app.InfoLog.Println(signedURL)
+
+	msg := Message{
+		To:       user.Email,
+		Subject:  "Activate your account",
+		Template: "confirmation-email",
+		Data:     template.HTML(signedURL),
+	}
+
+	app.sendEmail(msg)
+	app.Session.Put(r.Context(), "flash", "Confirmation Mail Sent from Our backend, Please check your Mail")
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 
 }
 
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/alexedwards/scs/redisstore v0.0.0-20231113091146-cef4b05350c8 // indirect
 	github.com/alexedwards/scs/v2 v2.7.0 // indirect
 	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/bwmarrin/go-alone v0.0.0-20190806015146-742bb55d1631 // indirect
 	github.com/go-chi/chi/v5 v5.0.11 // indirect
 	github.com/gomodule/redigo v1.8.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
@@ -24,5 +25,6 @@ require (
 	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
